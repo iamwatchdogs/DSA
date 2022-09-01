a962f045ac5dd1d0146696b403d60852fda09cc0
iamwatchdogs <shamith301102@gmail.com>
2022-09-01 23:12:32 +0530

    added commented output to Linear search

--- Search_Algo/Linear_Search.java
+++ Search_Algo/Linear_Search.java
@@ -3,6 +3,24 @@ package Search_Algo;
 import java.util.Arrays;
 
 public class Linear_Search {
+
+    // Basic iterative Linear search which return the index if the target element is found, else returns -1
+    public static int search_1(int[] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            if( arr[i] == target )
+                return i;
+        }
+        return -1;
+    }
+
+    // Basic iterative Linear search which return the element if the target element is found, else returns -1
+    public static int search_2(int[] arr, int target) {
+        for (int i : arr) {
+            if (i == target)
+                return i;
+        }
+        return -1;
+    }
     public static void main(String[] args) {
 
         int [] arr = {1, 55, 98, 89, 45, -6};
@@ -18,19 +36,16 @@ public class Linear_Search {
 
     }
 
-    public static int search_1(int[] arr, int target) {
-        for (int i = 0; i < arr.length; i++) {
-            if( arr[i] == target )
-                return i;
-        }
-        return -1;
-    }
-    public static int search_2(int[] arr, int target) {
-        for (int i : arr) {
-            if (i == target)
-                return i;
-        }
-        return -1;
-    }
 }
 
+//  Output:-
+//  -------
+//
+//        Return Index:
+//        Array: [1, 55, 98, 89, 45, -6]
+//        Target: 98
+//        Ans: 2
+//        Return value:
+//        Array: [1, 55, 98, 89, 45, -6]
+//        Target: 98
+//        Ans: 98

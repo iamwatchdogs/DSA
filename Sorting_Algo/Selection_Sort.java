6fe1cdf81292f9610af0a922373fc7db5e0046e7
iamwatchdogs <shamith301102@gmail.com>
2022-09-02 21:49:11 +0530

    Added Selection_Sort.java

---
+++ Sorting_Algo/Selection_Sort.java
@@ -0,0 +1,46 @@
+package Sorting_Algo;
+
+import java.util.Arrays;
+
+public class Selection_Sort {
+
+    public static void selection_Sort(int [] arr ) {
+
+        // we will start with selecting i-th element
+        for (int i = 0; i < arr.length; i++) {
+
+            // we will iterate and find the smaller element
+            // than the i-th element and swap with it, so that
+            // all elements towards the left-hand side are sorted
+            // in increasing order. This process continues until
+            // the length of sorted == length of given array.
+            for( int j = i+1; j < arr.length; j++ ) {
+
+                // Swapping the smaller element to bring it towards
+                // left-hand side i.e., sorted side.
+                if( arr[i] > arr[j] ){
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+                // End of if-statement
+            }
+            // By the end of one i-th iteration, we will have one
+            // element sorted in its position. And the elements which
+            // get sorted first are the smaller valued elements.
+        }
+        // End of sorting algorithm
+    }
+
+    public static void main(String[] args) {
+        int [] arr = { 7, 1, -9, 0, 78, -14, -15, 99};
+        System.out.println("Unsorted Array: " + Arrays.toString(arr));
+        selection_Sort(arr);
+        System.out.println("Sorted Array: " + Arrays.toString(arr));
+    }
+}
+
+//  Output:
+//  ------
+//        Unsorted Array: [7, 1, -9, 0, 78, -14, -15, 99]
+//        Sorted Array: [-15, -14, -9, 0, 1, 7, 78, 99]

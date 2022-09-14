6a1fbafbaff8e23d9a1814014ca7d58d85e6c8c8
iamwatchdogs <shamith301102@gmail.com>
2022-09-14 23:47:45 +0530

    Solved Pattern_1.java

--- Pattern_Printing_Optional_/Problems_Solved/Pattern_1.java
+++ Pattern_Printing_Optional_/Problems_Solved/Pattern_1.java
@@ -1,4 +1,27 @@
 package Pattern_Printing_Optional_.Problems_Solved;
 
 public class Pattern_1 {
+
+    public static void main(String[] args) {
+        int n = 5;
+        print_Pattern(n);
+    }
+
+    public static void print_Pattern(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
 }
+
+//  Output:-
+//  -------
+//        *****
+//        *****
+//        *****
+//        *****
+//        *****

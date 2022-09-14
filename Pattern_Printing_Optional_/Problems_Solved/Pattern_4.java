74085df8e46eff671bb5be86a4aa4970973a70be
iamwatchdogs <shamith301102@gmail.com>
2022-09-14 23:55:34 +0530

    Solved Pattern_4.java

--- Pattern_Printing_Optional_/Problems_Solved/Pattern_4.java
+++ Pattern_Printing_Optional_/Problems_Solved/Pattern_4.java
@@ -1,2 +1,27 @@
-package Pattern_Printing_Optional_.Problems_Solved;public class Pattern_4 {
+package Pattern_Printing_Optional_.Problems_Solved;
+
+public class Pattern_4 {
+
+    public static void main(String[] args) {
+        int n = 5;
+        print_Pattern(n);
+    }
+
+    public static void print_Pattern(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print( j + " ");
+            }
+            System.out.println();
+        }
+    }
+
 }
+
+//  Output:-
+//  -------
+//        1
+//        1 2
+//        1 2 3
+//        1 2 3 4
+//        1 2 3 4 5

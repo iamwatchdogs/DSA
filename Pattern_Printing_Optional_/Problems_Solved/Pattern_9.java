f75b69c05a1f7567bc26a76bf5df68a864bc13f3
iamwatchdogs <shamith301102@gmail.com>
2022-09-15 00:24:11 +0530

    Solved Pattern_9.java

--- Pattern_Printing_Optional_/Problems_Solved/Pattern_9.java
+++ Pattern_Printing_Optional_/Problems_Solved/Pattern_9.java
@@ -1,2 +1,36 @@
-package Pattern_Printing_Optional_.Problems_Solved;public class Pattern_9 {
+package Pattern_Printing_Optional_.Problems_Solved;
+
+public class Pattern_9 {
+
+    public static void main(String[] args) {
+        int n = 5;
+        print_Pattern(n);
+    }
+
+    public static void print_Pattern(int n) {
+
+        for (int i = 0; i < n; i++) {
+
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+
+            int max = n * 2 - i - 1;
+
+            for (int j = i; j < max  ; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+
 }
+
+//  Output:-
+//  -------
+//    *********
+//     *******
+//      *****
+//       ***
+//        *

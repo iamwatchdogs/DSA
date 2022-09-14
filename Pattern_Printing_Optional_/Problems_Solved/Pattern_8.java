ceeadf55d0b16d235706389f134d3660ddffbe13
iamwatchdogs <shamith301102@gmail.com>
2022-09-15 00:16:20 +0530

    Solved Pattern_8.java

--- Pattern_Printing_Optional_/Problems_Solved/Pattern_8.java
+++ Pattern_Printing_Optional_/Problems_Solved/Pattern_8.java
@@ -1,2 +1,37 @@
-package Pattern_Printing_Optional_.Problems_Solved;public class Pattern_8 {
+package Pattern_Printing_Optional_.Problems_Solved;
+
+public class Pattern_8 {
+
+    public static void main(String[] args) {
+        int n = 5;
+        print_Pattern(n);
+    }
+
+    public static void print_Pattern(int n) {
+
+        for (int i = 0; i < n; i++) {
+
+            for (int j = i; j < n-1; j++) {
+                System.out.print(" ");
+            }
+
+            int max = i*2 + 1;
+
+            for (int j = 0; j < max; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+
 }
+
+//  Output:-
+//  -------
+//            *
+//           ***
+//          *****
+//         *******
+//        *********
+

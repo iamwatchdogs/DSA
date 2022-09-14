db706d5983571b437b61186d21fb927c6bbcd6b3
iamwatchdogs <shamith301102@gmail.com>
2022-09-14 23:52:44 +0530

    Solved Pattern_3.java

--- Pattern_Printing_Optional_/Problems_Solved/Pattern_3.java
+++ Pattern_Printing_Optional_/Problems_Solved/Pattern_3.java
@@ -1,2 +1,28 @@
-package Pattern_Printing_Optional_.Problems_Solved;public class Pattern_3 {
+package Pattern_Printing_Optional_.Problems_Solved;
+
+public class Pattern_3 {
+
+    public static void main(String[] args) {
+        int n = 5;
+        print_Pattern(n);
+    }
+
+    public static void print_Pattern(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
 }
+
+//  Output:-
+//  -------
+//        *****
+//        ****
+//        ***
+//        **
+//        *
+

7be6ceff4d3aaa3cbac179c1a63856c31500ab5c
iamwatchdogs <shamith301102@gmail.com>
2022-09-15 22:33:31 +0530

    Solved ../Pattern_13.java

--- Pattern_Printing_Optional_/Problems_Solved/Pattern_13.java
+++ Pattern_Printing_Optional_/Problems_Solved/Pattern_13.java
@@ -1,2 +1,35 @@
-package Pattern_Printing_Optional_.Problems_Solved;public class Pattern_13 {
+package Pattern_Printing_Optional_.Problems_Solved;
+
+public class Pattern_13 {
+
+    public static void main(String[] args) {
+        int n = 5;
+        print_Pattern(n);
+    }
+
+    public static void print_Pattern(int n) {
+
+        for (int i = 0; i < n; i++) {
+
+            for (int j = 0; j < n - i; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 0; j <= i * 2; j++) {
+                System.out.printf("%s", ( j > 0 && j < i*2 && i != n-1) ? " " : "*");
+            }
+
+            System.out.println();
+        }
+
+    }
+
 }
+
+//  Output:-
+//  -------
+//          *
+//         * *
+//        *   *
+//       *     *
+//      *********

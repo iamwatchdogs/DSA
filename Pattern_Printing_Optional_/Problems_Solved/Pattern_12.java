1b6b32803b890ad2608428a302bd6d79a7932404
iamwatchdogs <shamith301102@gmail.com>
2022-09-15 22:17:22 +0530

    Solved ../Pattern_12.java

--- Pattern_Printing_Optional_/Problems_Solved/Pattern_12.java
+++ Pattern_Printing_Optional_/Problems_Solved/Pattern_12.java
@@ -1,2 +1,41 @@
-package Pattern_Printing_Optional_.Problems_Solved;public class Pattern_12 {
+package Pattern_Printing_Optional_.Problems_Solved;
+
+public class Pattern_12 {
+
+    public static void main(String[] args) {
+        int n = 5;
+        print_Pattern(n);
+    }
+
+    public static void print_Pattern(int n) {
+
+        for (int i = 0; i < n*2; i++) {
+
+            int max = ( i < n ) ? i : n*2 - i - 1;
+
+            for (int j = 0; j < max; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 0; j < n - max; j++) {
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
+    }
+
 }
+
+//  Output:-
+//  -------
+//    * * * * *
+//     * * * *
+//      * * *
+//       * *
+//        *
+//        *
+//       * *
+//      * * *
+//     * * * *
+//    * * * * *

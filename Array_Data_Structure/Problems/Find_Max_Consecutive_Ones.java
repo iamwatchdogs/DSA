b75f2c7fb802260ef76b65610eb9c4dc46db5356
SAM-Learning-git <learngitfromsam@gmail.com>
2022-10-05 22:58:09 +0530

    Solved Find_Max_Consecutive_Ones.java

--- Array_Data_Structure/Problems/Find_Max_Consecutive_Ones.java
+++ Array_Data_Structure/Problems/Find_Max_Consecutive_Ones.java
@@ -1,4 +1,66 @@
 package Array_Data_Structure.Problems;
 
-public class Find_Max_Consecutive_Ones {
+class Beginner_Solution_1 {
+
+    // Time Complexity: O(n)
+    // Space Complexity: O(1)
+    public int worstCase ( int [] nums ) {
+
+        int max = 0, count = 0;
+
+        for( int i : nums ){
+
+            if( i == 1 ) {
+                ++count;
+                max = Math.max( max, count);
+            }
+
+            else {
+                count = 0;
+            }
+
+        }
+        return max;
+    }
+
+    // Time Complexity: O(n)
+    // Space Complexity: O(1)
+    public int bestCase ( int [] nums ) {
+
+        int max = 0, count = 0;
+
+        for( int i : nums ){
+
+            if( i == 0 ) {
+                max = Math.max( max, count);
+                count = 0;
+            }
+
+            else {
+                ++count;
+            }
+
+        }
+
+        return Math.max(max, count);
+    }
 }
+
+public class Find_Max_Consecutive_Ones {
+
+    public static void main(String[] args) {
+
+        int [] test_case_1 = {1,1,0,1,1,1};
+        int [] test_case_2 = {1,0,1,1,0,1};
+
+        Beginner_Solution_1 sol = new Beginner_Solution_1();
+
+        System.out.println( sol.worstCase(test_case_1) == 3 );
+        System.out.println( sol.bestCase(test_case_2) == 2 );
+
+    }
+}
+
+// Output:
+//      true
+//      true
